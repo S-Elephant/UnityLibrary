b376c94d420a96b857625a4792aa52b901fd46e3
SquirtingElephant <75314121+SquirtingElephant@users.noreply.github.com>
2023-08-12 20:32:13 +0200

    Add CoroutineStarter

---
+++ Other/CoroutineStarter.cs
@@ -0,0 +1,33 @@
+﻿#nullable enable
+using UnityEngine;
+
+namespace Elephant.UnityLibrary.Other
+{
+	/// <summary>
+	/// A way to start coroutines from non-MonoBehaviour classes in Unity.
+	/// </summary>
+	/// <example>CoroutineStarter.Instance.StartCoroutine(MyCoroutine)</example>
+	public class CoroutineStarter : MonoBehaviour
+	{
+		/// <summary>
+		/// Singleton instance.
+		/// </summary>
+		private static CoroutineStarter? _instance = null;
+
+		/// <summary>
+		/// Public access singleton instance.
+		/// </summary>
+		public static CoroutineStarter Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					GameObject go = new("CoroutineStarter");
+					_instance = go.AddComponent<CoroutineStarter>();
+				}
+				return _instance;
+			}
+		}
+	}
+}
