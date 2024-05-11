644c82c7f6070601c820d1542bf572afc85a0c0f
Elephant <75314121+S-Elephant@users.noreply.github.com>
2024-05-11 19:00:06 +0200

    Extra optional ToSprite() parameter

--- Elephant.UnityLibrary/Extensions/Texture2DExtensions.cs
+++ Elephant.UnityLibrary/Extensions/Texture2DExtensions.cs
@@ -10,9 +10,9 @@ namespace Elephant.UnityLibrary.Extensions
 		/// <summary>
 		/// Create and return a new <see cref="Sprite"/> from a <see cref="Texture2D"/>.
 		/// </summary>
-		public static Sprite ToSprite(this Texture2D texture2D, float pivotX = 0.5f, float pivotY = 0.5f)
+		public static Sprite ToSprite(this Texture2D texture2D, float pivotX = 0.5f, float pivotY = 0.5f, float pixelsPerUnit = 100f, uint extrude = 0)
 		{
-			return Sprite.Create(texture2D, new Rect(0.0f, 0.0f, texture2D.width, texture2D.height), new Vector2(pivotX, pivotY));
+			return Sprite.Create(texture2D, new Rect(0.0f, 0.0f, texture2D.width, texture2D.height), new Vector2(pivotX, pivotY), pixelsPerUnit, extrude);
 		}
 	}
 }
